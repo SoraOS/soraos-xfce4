8bd176352b974cc6c3f32ed1d9273546630db283
p3nguin <p3nguinkun@proton.me>
2024-04-23 12:11:57 +0700

    no

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # SoraOS Xfce4 theming
 
 install_packages(){
-	sudo pacman -S --noconfirm --needed xfce4 xfce4-goodies arc-gtk-theme papirus-icon-theme
+	sudo pacman -S --noconfirm --needed xfce4 xfce4-goodies arc-gtk-theme papirus-icon-theme ttf-fira-sans ttf-fira-code
 }
 
 copy_files(){
